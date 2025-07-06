33f3628cef810f089ee3022cab31a507804a5c7a
erab2r <shkera213@gmail.com>
2025-07-07 02:35:35 +0600

    we

--- index1.html/js/events.js
+++ index1.html/js/events.js
@@ -1,5 +1,13 @@
 
-//option: 2
+//option: 2 add onclick function
 function makeRed(){
-            document.body.style.backgroundColor = 'red'
-        }
+     document.body.style.backgroundColor = 'red'
+}
+
+// option: 3 
+const makeBlueButton = document.getElementById('make-Blue');
+      makeBlueButton.onclick = makeBlue;
+      function makeBlue(){
+        document.body.style.backgroundColor = 'blue';
+      }
+
